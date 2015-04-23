4be17d6715ae6a255753cd0a7a21baeb11795fce
Adam Gallinat <adamgallinat@gmail.com>
2015-04-23 10:49:25 -0400

    heroku issues

--- models/index.js
+++ models/index.js
@@ -9,6 +9,17 @@ var config    = require(__dirname + '/../config/config.json')[env];
 var sequelize = new Sequelize(config.database, config.username, config.password, config);
 var db        = {};
 
+if (process.env.HEROKU_POSTGRESQL_RED_URL) {
+  // the application is executed on Heroku ... use the postgres database
+  sequelize = new Sequelize(process.env.HEROKU_POSTGRESQL_RED_URL, {
+    dialect:  'postgres',
+    protocol: 'postgres',
+    logging:  true //false
+  })
+} else {
+  var sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
+
 fs
   .readdirSync(__dirname)
   .filter(function(file) {
